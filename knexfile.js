50ec85b8bca7df739e9c6c6db34dc88f2b7cf0e2
Rod Paavola <rpaavola@mac.com>
2023-05-12 17:27:46 +0300

    updating

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,17 @@
+require('dotenv').config();
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+
 module.exports = {
-    client: "mysql",
-    connection: {
-      host: "127.0.0.1",
-      user: "root",
-      password: "rootroot",
-      database: "grubGrabber",
-      charset: "utf8",
-    },
-  };
-  
+  client: "mysql",
+  connection: {
+    host: "127.0.0.1",
+    user: process.env.DB_LOCAL_USER,
+    password: process.env.DB_LOCAL_PASSWORD,
+    database: process.env.DB_LOCAL_DBNAME,
+    charset: "utf8",
+  },
+};
